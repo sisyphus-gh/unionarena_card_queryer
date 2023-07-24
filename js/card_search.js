d1642711c0b8f454cc8310a199eab630222c0fb3
jiangjunjie <1620657419@qq.com>
2023-07-24 18:13:32 +0800

    手机端改造

--- js/card_search.js
+++ js/card_search.js
@@ -1,8 +1,13 @@
 let mode = "prd";
 
+
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".materialboxed");
   var instances = M.Materialbox.init(elems);
+
+
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
 });
 
 var datas;
@@ -13,8 +18,15 @@ request.send(null); /*不发送数据到服务器*/
 request.onload = function () {
   /*XHR对象获取到返回信息后执行*/
   if (this.status == 200) {
+    let queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    let ip = urlParams.get("ip");
+
     /*返回状态为200，即为数据获取成功*/
     datas = JSON.parse(this.responseText);
+    datas = datas.filter((data) => {
+      return data.ipData == ip && !data.rareData.includes("★");
+    });
 
     let cardDivHtml = "";
     let cardDivHtml2 = "";
@@ -23,7 +35,7 @@ request.onload = function () {
       if (mode == "dev") {
         data.imageUrl = "./card/" + data.imageFileName;
       }
-     
+
       cardDivHtml =
         cardDivHtml +
         `<div class=" " style=" width: 16.6666667%;">
@@ -58,9 +70,15 @@ request.onload = function () {
     /*返回状态为200，即为数据获取成功*/
     let response = JSON.parse(this.responseText);
 
-    let ipDataSelect = document.querySelector('[name="ipData"]');
+    let sidenavIp = document.getElementById("sidenav-ip");
     response.ips.forEach((element) => {
-      ipDataSelect.options.add(new Option(element.ipName, element.serialNo));
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      a.href = "?ip=" + element.serialNo;
+      a.innerHTML = element.ipName;
+      li.appendChild(a);
+
+      sidenavIp.appendChild(li);
     });
   }
 };
@@ -104,7 +122,6 @@ function clickCard(e) {
 function search() {
   form = document.getElementById("search");
   let effectData = form.querySelector('[name="effectData"]').value;
-  let ipData = form.querySelector('[name="ipData"]').value;
   let cardCnName = form.querySelector('[name="cardCnName"]').value;
   let cardPackData = form.querySelector('[name="cardPackData"]').value;
   let colorData = form.querySelector('[name="colorData"]').value;
@@ -139,9 +156,7 @@ function search() {
       continue;
     }
 
-    if (ipData != "" && data.ipData != ipData) {
-      continue;
-    }
+
     if (cardPackData != "" && !data.cardPackData.endsWith(cardPackData)) {
       continue;
     }
@@ -225,5 +240,138 @@ function search() {
   document.getElementById("cardListArea").innerHTML = cardDivHtml;
   document.getElementById("cardListArea2").innerHTML = cardDivHtml2;
 
-  document.getElementById("searchCount").innerHTML = count;
+
+  Array.prototype.forEach.call(document.getElementsByClassName("searchCount"), element => {
+    element.innerHTML = count;
+  });
+
+}
+
+
+function search2() {
+  form = document.getElementById("mobile-demo");
+  let effectData = form.querySelector('[name="effectData"]').value;
+  let cardCnName = form.querySelector('[name="cardCnName"]').value;
+  let cardPackData = form.querySelector('[name="cardPackData"]').value;
+  let colorData = form.querySelector('[name="colorData"]').value;
+  let needEnergyData = form.querySelector('[name="needEnergyData"]').value;
+  let cardNumData = form.querySelector('[name="cardNumData"]').value;
+  let rareData = form.querySelector('[name="rareData"]').value;
+  let parallelFlag = form.querySelector('[name="parallelFlag"]').value;
+  let triggerCnData = form.querySelector('[name="triggerCnData"]').value;
+  let rareStarData = form.querySelector('[name="rareStarData"]').value;
+  let categoryData = form.querySelector('[name="categoryData"]').value;
+
+  let cardList = [];
+
+  for (let i = 0; i < datas.length; i++) {
+    let data = datas[i];
+    if (effectData != "") {
+      if (
+        !data.effectCnData.includes(effectData) &&
+        !data.attributeCnData.join("$$").includes(effectData)
+      ) {
+        continue;
+      }
+    }
+    if (cardNumData != "" && !data.cardNumData.includes(cardNumData)) {
+      continue;
+    }
+    if (
+      cardCnName != "" &&
+      !data.cardCnName.includes(cardCnName) &&
+      !data.cardName.includes(cardCnName)
+    ) {
+      continue;
+    }
+
+
+    if (cardPackData != "" && !data.cardPackData.endsWith(cardPackData)) {
+      continue;
+    }
+    if (colorData != "" && !data.needEnergyData.includes(colorData)) {
+      continue;
+    }
+    if (needEnergyData != "" && !data.needEnergyData.endsWith(needEnergyData)) {
+      continue;
+    }
+    if (parallelFlag != "" && data.rareData.includes("★")) {
+      continue;
+    }
+    if (rareData != "" && data.rareData != rareData) {
+      continue;
+    }
+    if (triggerCnData != "" && !data.triggerCnData.startsWith(triggerCnData)) {
+      continue;
+    }
+    if (rareStarData != "") {
+      if (rareStarData == "★★★") {
+        if (!data.rareData.endsWith(rareStarData)) {
+          continue;
+        }
+      } else if (rareStarData == "★★") {
+        if (
+          !data.rareData.endsWith(rareStarData) ||
+          data.rareData.endsWith("★★★")
+        ) {
+          continue;
+        }
+      } else if (rareStarData == "★") {
+        if (
+          !data.rareData.endsWith(rareStarData) ||
+          data.rareData.endsWith("★★")
+        ) {
+          continue;
+        }
+      }
+    }
+    if (categoryData != "" && data.categoryData != categoryData) {
+      continue;
+    }
+    cardList.push(data);
+  }
+
+  cardList = cardList.sort(function (data1, data2) {
+    return data1.cardNumData2 > data2.cardNumData2
+      ? 1
+      : data1.cardNumData2 == data2.cardNumData2
+        ? 0
+        : -1;
+  });
+
+  let count = 0;
+  let cardDivHtml = "";
+  let cardDivHtml2 = "";
+
+  cardList.forEach((data) => {
+    cardDivHtml =
+      cardDivHtml +
+      `<div class=" " style=" width: 16.6666667%;">
+            <img onclick="clickCard(this);" id="${data.id}" src="${data.imageUrl}" width="100%" class="col s12"/>
+            <p class="center-align" style="font-size: 1.1rem; margin: 0px;">${data.cardNumData}</p>
+            <p class="center-align " style="font-size: 1rem; margin: 0px;">${data.cardCnName}</p>
+        </div>
+        
+    `;
+    cardDivHtml2 =
+      cardDivHtml2 +
+      `<div class="center-align" style="width: 33.3333%;">
+            <a target="_blank" href="./info.html?cardNumData=${data.cardNumData}" >
+                <img src="${data.imageUrl}" width="80%" style="margin: 0 auto;" />
+                <p style="font-size: 1rem;margin: 0px;">${data.cardNumData}</p>
+                <p style="margin: 0px;">${data.cardCnName}</p>
+            </a>
+        </div>
+    `;
+    count++;
+  });
+
+  document.getElementById("cardListArea").innerHTML = cardDivHtml;
+  document.getElementById("cardListArea2").innerHTML = cardDivHtml2;
+
+
+  Array.prototype.forEach.call(document.getElementsByClassName("searchCount"), element => {
+    element.innerHTML = count;
+  });
+
 }
